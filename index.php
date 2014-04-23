5bf02171f4f43725b6d2187707a8d530cbc62d6f
Lee Jordan <ldjordan@gmail.com>
2014-04-23 17:31:30 +0100

    fixing the offline testing comments

--- index.php
+++ index.php
@@ -8,12 +8,12 @@
     <body>
         <?php
             require_once('php/minimap.php');
-            minimap\renderMiniMapFromUrl();
+            //minimap\renderMiniMapFromUrl();
 
             // for offline testing
-            // $building_status = minimap::getBuildingStatusAsArray(1974, 0, 63, 0);
-            // $building_status = minimap::getBuildingStatusAsArray(388, 51, 1975, 631);
-            // minimap::renderMiniMapFromArray($building_status);
+            // $building_status = minimap\getBuildingStatusAsArray(1974, 0, 63, 0);
+            // $building_status = minimap\getBuildingStatusAsArray(388, 51, 1975, 631);
+            // minimap\renderMiniMapFromArray($building_status);
 
             // Further reading:
             // http://wiki.teamfortress.com/wiki/WebAPI/GetMatchDetails#Tower_Status
